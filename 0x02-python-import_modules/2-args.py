7f1af540247a11fe3e237af4471891d2445a3589
chikizy <chijinduonyemelukwe001@gmail.com>
2024-04-14 19:56:39 +0100

    new

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-if __name__ ==" __main__"
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-if len(argv) < 2:
-    print('0 arguments.')
-elif len(argv) == 2:
-    print('1 argument:')
-elif len(argv) > 2:
-    print("{:d} arguments:".format(len(argv) - 1))
-for i in range(1, len(argv)):
-    print('{:d}: {}'.format(i, argv[i]))
+    if len(argv) < 2:
+        print('{:d} arguments.'.format(len(argv) - 1))
+    elif len(argv) == 2:
+        print('{:d} argument:'.format(len(argv) - 1))
+    elif len(argv) > 2:
+        print("{:d} arguments:".format(len(argv) - 1))
+    for i in range(1, len(argv)):
+        print('{:d}: {}'.format(i, argv[i]))
