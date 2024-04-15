1bc2ac69a95562844cdbe170e8ac9095ec9938ae
chikizy <chijinduonyemelukwe001@gmail.com>
2024-04-15 23:39:07 +0100

    done

--- 0x02-python-import_modules/4-hidden_discovery1.py
+++ 0x02-python-import_modules/4-hidden_discovery1.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import hidden_4
-    for name in dir(hidden_4):
-        if name.startswith("__"):
+
+    for i in dir(hidden_4):
+        if i.startswith("__"):
             continue
         else:
-            print(name)
+            print(i)
