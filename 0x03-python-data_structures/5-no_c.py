f814b4516569cf6888477689cfc0415e46c6a241
chikizy <chijinduonyemelukwe001@gmail.com>
2024-04-30 13:05:21 +0100

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,6 @@
-#!/usr/bin/python3
-
-
 def no_c(my_string):
-    copy_str = ""
+    str = ''
     for i in my_string:
-        if i == 'c' or i == 'C':
-            copy_str += ""
-        else:
-            copy_str += i
-    return copy_str
+        if i != 'c' and i != 'C':
+            str += i
+    return str
